8e123866dd443b5c4ca32af29cbbcb2d43d47f6b
Bonface Masira <bonfacemasira@gmail.com>
2022-08-02 03:29:15 +0300

    create project structure and install tailwind css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import tailwindConfig from '../tailwind.config';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <p>
+        Welcome!
+      </p>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <p className="text-gray-500 text-lg">
+        React and Tailwind CSS in action
+      </p>
     </div>
   );
 }
