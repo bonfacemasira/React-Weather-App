eaa228f29b5e2f2d599fcfc3f3ba20a9b233f4bd
Bonface Masira <bonfacemasira@gmail.com>
2022-08-07 02:28:36 +0300

    define api and key

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+    let key = '8951b1f9e1c84a99b24231509220608';
+    let url = 'http://api.weatherapi.com/v1/current.json?key=8951b1f9e1c84a99b24231509220608&q=London&aqi=no';
+
+    return(
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export default Home;
